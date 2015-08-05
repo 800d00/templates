72d4d1c3ca8f2b2fd536c6ede9e0d66159edc711
alex.safonov <madriva@mail.ru>
2015-08-05 16:10:14 +0300

    First commit

---
+++ MWA/App_Start/BundleConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MWA
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-2.1.4/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/validate/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/flexslider").Include(
+            "~/Scripts/jquery.flexslider.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap/bootstrap.js",
+                      "~/Scripts/Site.js",
+                      "~/Scripts/respond/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fancybox").Include(
+                      "~/Scripts/fancybox/jquery.fancybox.js",
+                      "~/Scripts/fancybox/jquery.fancybox*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      //"~/Content/bootstrap/bootstrap.css",
+                      "~/Content/bootstrap/bootstrap*",
+                      "~/Content/Site.css",
+                      //"~/Content/fancybox/jquery.fancybox.css",
+                      "~/Content/Clouds.css",
+                      "~/Content/fancybox/jquery.fancybox*",
+                      "~/Content/Site-Responsive.css",
+                      "~/Content/font-awesome/font-awesome.css",
+                      "~/Content/flexslider/flexslider.css"));
+        }
+    }
+}
---
+++ MWA/obj/Debug/AspnetCompileMerge/TempBuildDir/Scripts/Site.js
@@ -0,0 +1,10 @@
+﻿var invocation = new XMLHttpRequest();
+var url = 'http://localhost';
+
+function callOtherDomain() {
+    if (invocation) {
+        invocation.open('GET', url, true);
+        invocation.onreadystatechange = handler;
+        invocation.send();
+    }
+}
